90ff579252940411d97703fc7527963bbaf2adcb
sajeerAhamed15 <ahamedsajeer.15@cse.mrt.ac.lk>
2020-03-22 12:40:45 +0530

    change port number

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || process.argv[2] || 8080;
+
 
 const server = http.createServer(function(req, res) {
   res.statusCode = 200;
@@ -9,6 +10,6 @@ const server = http.createServer(function(req, res) {
   res.end('Hello World\n');
 });
 
-server.listen(port, hostname, function() {
-  console.log('Server running at http://'+ hostname + ':' + port + '/');
+server.listen(port, function() {
+  console.log('Server running at:' + port + '/');
 });
